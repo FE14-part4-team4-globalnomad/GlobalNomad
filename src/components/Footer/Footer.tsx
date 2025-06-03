86b77f9f9704205fb386063448e47fd136e2cdb8
HyunseokLEE <unducklee@gmail.com>
2025-06-03 18:40:32 +0900

    [GN-24] feat: 공동 컴포넌트 footer mobile ui 완성

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,45 +5,67 @@ import InstagramIcon from "@/assets/image/icons/socialicon/icon_instagram.svg";
 import XIcon from "@/assets/image/icons/socialicon/icon_X.svg";
 import YoutubeIcon from "@/assets/image/icons/socialicon/icon_youtube.svg";
 
+const socialIcons = [
+  { href: "https://www.facebook.com/", src: FacebookIcon, alt: "Facebook" },
+  { href: "https://www.instagram.com/", src: InstagramIcon, alt: "Instagram" },
+  { href: "https://www.youtube.com/", src: YoutubeIcon, alt: "YouTube" },
+  { href: "https://x.com/", src: XIcon, alt: "X" },
+];
+
 export default function Footer() {
   return (
     <footer className="w-full my-5 bg-white text-[13px] text-[color:var(--gray-400)]">
-      <div className="w-[90%] mx-auto flex justify-between items-center gap-3">
-        <div className="flex-shrink-0">©codeit - 2023</div>
-        <div className="flex gap-2">
-          <a href="#" className="text-[color:var(--gray-400)]">
+      <div
+        className="
+          w-[90%] mx-auto
+          grid grid-cols-3 grid-rows-2
+          sm:grid-rows-1 sm:grid-cols-3
+          items-center gap-2
+          text-center sm:text-left
+        "
+      >
+        <div
+          className="
+            row-start-1 col-span-3
+            flex justify-center gap-2
+            sm:col-span-1 sm:col-start-2 sm:row-start-1
+          "
+        >
+          <a href="#" className="text-[color:var(--gray-600)]">
             Privacy Policy
           </a>
           <span>·</span>
-          <a href="#" className="text-[color:var(--gray-400)]">
+          <a href="#" className="text-[color:var(--gray-600)]">
             FAQ
           </a>
         </div>
-        <div className="flex gap-1">
-          <a href="https://www.facebook.com/">
-            <Image
-              src={FacebookIcon}
-              alt="Facebook"
-              className="w-[2rem] h-[2rem]"
-            />
-          </a>
-          <a href="https://www.instagram.com/">
-            <Image
-              src={InstagramIcon}
-              alt="Instagram"
-              className="w-[2rem] h-[2rem]"
-            />
-          </a>
-          <a href="https://www.youtube.com/">
-            <Image
-              src={YoutubeIcon}
-              alt="YouTube"
-              className="w-[2rem] h-[2rem]"
-            />
-          </a>
-          <a href="https://x.com/">
-            <Image src={XIcon} alt="X" className="w-[2rem] h-[2rem]" />
-          </a>
+
+        <div
+          className="
+            row-start-2 col-start-1
+            flex justify-start
+            sm:row-start-1 sm:col-start-1
+          "
+        >
+          <span>©codeit - 2023</span>
+        </div>
+
+        <div
+          className="
+            row-start-2 col-start-3
+            flex justify-end gap-1
+            sm:row-start-1 sm:col-start-3
+          "
+        >
+          {socialIcons.map(({ href, src, alt }) => (
+            <a href={href} key={alt}>
+              <Image
+                src={src}
+                alt={alt}
+                className="w-[2rem] h-[2rem] filter grayscale brightness-75 opacity-70 sm:filter-none sm:brightness-100 sm:opacity-100"
+              />
+            </a>
+          ))}
         </div>
       </div>
     </footer>
