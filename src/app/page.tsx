df268f8a9cf81b91d26de78c3705fc4b43a8f3ac
김태욱 <idididsjkim@gmali.com>
2025-06-04 08:02:00 +0900

    [GN-111] feat: 페이지네이션 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import Gnb from "@/components/gnb/Gnb";
-import Logo from "@/components/logo/Logo";
-
 export default function Home() {
   return (
     <div className="flex flex-row sm:flex-col items-center justify-center gap-[16px] min-h-screen text-20-body-b">
-      <Gnb />
-      <Logo />
       <div>Hello,</div>
       <div className="p-[0.8rem] sm:pb-[20px] text-32-b text-brand-500">
         GlobalNomad
