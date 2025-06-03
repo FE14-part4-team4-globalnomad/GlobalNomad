3ab7596c8064d38f377030ff0d4b10c9df736dd1
dkozowlk <56295839+dkozowlk@users.noreply.github.com>
2025-06-03 20:47:04 +0900

    [GN-104] 로고 컴포넌트 반응형과 클릭시 "/" 페이지로 이동 기능 구현 (#12)
    * [GN-26] rename: 아이콘 파일 위치 수정(/src/assets/icons) 및 폴더명 수정
    
    * [GN-26] style: 폰트 굵기 600 추가
    
    * [GN-26] feat: 공통 컴포넌트 Card (메인 페이지용) 추가 및 스토리 작성
    
    * [GN-26] rename: 숫자 포맷 유틸 함수명 변경
    
    * [GN-26] feat: 공통 컴포넌트 MyReservationCard (예약내역 용) 초안 추가 및 스토리 생성
    
    * [GN-26] style: 공통 컴포넌트 Card, MyReservationCard 금액 영역 폰트 크기 수정
    
    * [GN-26] feat: 공통 컴포넌트 MyExperienceCard (내 체험 관리 용) 추가 및 스토리 작성
    
    * [GN-26] chore: 공통 컴포넌트 Card 스토리 제목 변경
    
    * [GN-23] style: gnb 로그인후와 로그인전 ui 및 반응형 구현
    
    * [GN-23] rename: 이미지 파일 명 변경 및 위치 복구
    
    * [GN-104] feat: Logo component 개발
    
    ---------
    
    Co-authored-by: callu9 <callu_9ine@naver.com>
    Co-authored-by: 김태욱 <idididsjkim@gmali.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-import Gnb from "@/components/gnb/Gnb";
-
 export default function Home() {
   return (
     <div className="flex flex-row sm:flex-col items-center justify-center gap-[16px] min-h-screen text-20-body-b">
-      <Gnb />
       <div>Hello,</div>
       <div className="p-[0.8rem] sm:pb-[20px] text-32-b text-brand-500">
         GlobalNomad
