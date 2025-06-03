16e7e77fe2b077cccf2f066145100c686f136d79
LeeJiEuns <87702194+LeeJiEuns@users.noreply.github.com>
2025-06-03 09:31:35 +0900

    [GN-110] vercel 오류 수정

--- src/stories/calendarDate/CalendarDay.stories.tsx
+++ src/stories/calendarDate/CalendarDay.stories.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import type { Meta, StoryObj } from '@storybook/nextjs';
+import React from "react";
 
 import { CalendarDay } from "../../components/calendarDate/CalendarDay";
 import { CalendarEventBadge } from "../../components/calendarDate/CalendarEventBadge";
