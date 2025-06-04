f2c7662843f1a84e5ba70d35574413d13bf89ae6
LeeJiEuns <adc6511010@gmail.com>
2025-06-04 15:28:50 +0900

    [GN-134] not-found 페이지 반응형 제작

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,21 +1,39 @@
 'use client';
 
-import Earth from '@/assets/images/notFound/404_earth.svg';
-import Face from '@/assets/images/notFound/404_earth_face.svg';
+import { useRouter } from 'next/navigation';
+import WarningIcon from '@/assets/images/warning.svg';
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <div className="relative w-full h-screen flex flex-col items-center justify-center">
-      {/* 지구와 표정을 함께 감싸는 div */}
-      <div className="relative inline-block">
-        <Earth className="block" />
-        <Face className="absolute inset-0 w-full h-full" />
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="flex items-center">
+        <span className="text-[138px] mobile:text-[60px] font-bold text-gray-900">4</span>
+        <div className="mx-4 mobile:mx-[1px] scale-200 mobile:scale-100 origin-center flex-shrink-0">
+          <WarningIcon />
+        </div>
+        <span className="text-[138px] mobile:text-[60px] font-bold text-gray-900">4</span>
       </div>
 
-      {/* 메시지 */}
-      <div className="mt-10 text-center">
-        <h1 className="text-4xl font-bold text-gray-800 mb-2">404 - 페이지를 찾을 수 없어요</h1>
-        <p className="text-gray-500">지구가 울고 있어요... 돌아가 주세요.</p>
+      <div className="text-center">
+        <h1 className="text-32-b mobile:text-16-b text-gray-800">현재 찾을 수 없는 페이지를 요청했어요!</h1>
+        <h2 className="text-18-m mobile:text-11-m text-gray-600 my-2 mobile:my-1">요청하신 페이지가 사라졌거나 존재하지 않아요. 돌아가는 건 어떨까요?</h2>
+        <div className="font-semibold flex gap-2 justify-center mobile:flex-col">
+          <button
+            onClick={() => router.push('/')}
+            className="px-6 py-2 bg-brand-500 text-white rounded-3xl shadow cursor-pointer mobile:w-full"
+          >
+            메인 페이지
+          </button>
+
+          <button
+            onClick={() => router.back()}
+            className="px-6 mobile:px-5 py-2 bg-white text-gray-600 rounded-3xl shadow cursor-pointer mobile:w-full"
+          >
+            이전 페이지
+          </button>
+        </div>
       </div>
     </div>
   );
