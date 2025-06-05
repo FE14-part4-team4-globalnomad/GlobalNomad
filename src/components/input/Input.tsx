7d039f69a874985cfe25e32ab131394dd9e65286
LeeJiEuns <87702194+LeeJiEuns@users.noreply.github.com>
2025-06-05 12:02:58 +0900

    [Gn-108] vercel 배포 오류 수정 (#21)
    * [GN-108] Search 컴포넌트 생성
    
    * [GN-108] package.json package-lock.json 설치
    
    * [GN-108] 오류 수정
    
    * [GN-108] vercel 오류 수정
    
    * [GN-108] 아이콘 및 useRef로 변경
    
    * [GN-108] vercel 배포 오류 수정
    
    * [GN-108] Search 임포트 순서 변경
    
    * [GN-108] Search 컴포넌트 오류 수정
    
    * [GN-114] Input 컴포넌트 임포트 위치 변경
    
    * [GN-108] Search 컴포넌트 배포 오류 수정

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -13,11 +13,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
 
     return (
       <div className="flex flex-col">
-        {label && (
-          <label htmlFor={id} className="text-16-m text-gray-950 mb-1">
-            {label}
-          </label>
-        )}
+        {label && <label htmlFor={id} className="text-16-m text-gray-950 mb-1">{label}</label>}
         <div className="relative">
           <input
             id={id}
@@ -45,10 +41,12 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
             </button>
           )}
         </div>
-        {error && <p className="text-red-500 text-12-m mt-[6px]">{error}</p>}
+        {error && (
+          <p className="text-red-500 text-12-m mt-[6px]">{error}</p>
+        )}
       </div>
     );
-  },
+  }
 );
 
 Input.displayName = "Input";
