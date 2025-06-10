e1459f6e6f3151426c64920022082e96bb850b96
callu9 <callu_9ine@naver.com>
2025-06-10 12:13:59 +0900

    [GN-107] chore: 검색 소팅 필터 오타 수정

--- src/constants/activity.ts
+++ src/constants/activity.ts
@@ -7,8 +7,8 @@ export const ACTIVITY_CATEGORIES = [
   "웰빙",
 ];
 export const ACTIVITY_SORT_FILTER = [
-  { id: "latest", title: "최신 등록 순" },
-  { id: "price_asc", title: "가격 높은 순" },
-  { id: "price_desc", title: "가격 낮은 순" },
+  { id: "latest", title: "최신순" },
+  { id: "price_asc", title: "가격 낮은 순" },
+  { id: "price_desc", title: "가격 높은 순" },
   { id: "most_reviewed", title: "인기순" },
 ];
