2219436febfb4f1004f535986cd7dd7d10ecbff6
callu9 <callu_9ine@naver.com>
2025-06-12 19:57:22 +0900

    [GN-129] chore: next config 주석 제거

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  /* https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/profile_image/14-4_1912_1749718020858.jpeg */
   images: {
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "**.sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
-    //   },
-    // { protocol: "http", hostname: "**.domain.co(m|\.kr)" },
-    // ],
-
     remotePatterns: [
       new URL(
         "https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/**",
