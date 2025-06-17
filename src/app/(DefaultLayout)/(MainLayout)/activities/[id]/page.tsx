b5b27fbc4937eea98cf099a491fdc68a877a291d
LeeJiEuns <adc6511010@gmail.com>
2025-06-18 03:44:33 +0900

    [GN-128] ActivityDetailPage 타입 추론 오류 수정

--- src/app/(DefaultLayout)/(MainLayout)/activities/[id]/page.tsx
+++ src/app/(DefaultLayout)/(MainLayout)/activities/[id]/page.tsx
@@ -2,6 +2,6 @@ import ActivityDetailPage from './components/ActivityDetailPage';
 
 export const dynamic = 'force-dynamic';
 
-export default function Page(props: { params: { id: string } }) {
-  return <ActivityDetailPage {...props} />;
+export default function Page({ params }: { params: { id: string } }) {
+  return <ActivityDetailPage params={params} />;
 }
