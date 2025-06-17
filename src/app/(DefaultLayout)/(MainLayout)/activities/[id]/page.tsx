ff2a300880da3f3a1fc481d691e76950a6ecd960
LeeJiEuns <87702194+LeeJiEuns@users.noreply.github.com>
2025-06-18 02:49:29 +0900

    [GN-128] generateStaticParams 정의 및 오류 수정

--- src/app/(DefaultLayout)/(MainLayout)/activities/[id]/page.tsx
+++ src/app/(DefaultLayout)/(MainLayout)/activities/[id]/page.tsx
@@ -1,5 +1,9 @@
 import ClientActivityDetail from './components/ClientActivityDetail';
 
+export async function generateStaticParams() {
+  return [];
+}
+
 export default async function Page({ params }: { params: { id: string } }) {
   const activityId = Number(params.id);
   return <ClientActivityDetail activityId={activityId} />;
