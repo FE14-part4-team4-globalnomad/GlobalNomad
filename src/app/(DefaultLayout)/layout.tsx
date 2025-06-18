a19c154ab2aab048e665d990f6e8153814350214
HyunseokLEE <unducklee@gmail.com>
2025-06-18 17:30:43 +0900

    [GN-130] fix: 메인 페이지 레이아웃 코드 수정

--- src/app/(DefaultLayout)/layout.tsx
+++ src/app/(DefaultLayout)/layout.tsx
@@ -1,11 +1,22 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 
 import Footer from "@/components/footer/Footer";
 import Gnb from "@/components/gnb/Gnb";
+import { cn } from "@/utils/classNames";
 
 function Layout({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+
   return (
-    <div className="w-screen min-h-screen flex flex-col gap-[4rem] bg-main bg-cover">
+    <div
+      className={cn(
+        "w-screen min-h-screen flex flex-col gap-[4rem]",
+        pathname === "/" && "bg-main bg-cover",
+      )}
+    >
       <Gnb />
       <div className="flex flex-col flex-1">{children}</div>
       <Footer />
