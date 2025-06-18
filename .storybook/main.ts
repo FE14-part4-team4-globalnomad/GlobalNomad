0ea5f4941cede738c3aa8f994d05a73ea985cce0
callu9 <callu_9ine@naver.com>
2025-06-19 05:10:22 +0900

    [GN-138] chore: svgr 사용을 위한 환경설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -10,7 +10,7 @@ const config: StorybookConfig = {
     "@storybook/addon-vitest",
   ],
   framework: {
-    name: "@storybook/nextjs-vite",
+    name: "@storybook/nextjs",
     options: {},
   },
   staticDirs: ["../public"],
@@ -34,5 +34,22 @@ const config: StorybookConfig = {
     }
   </style>
   `,
+  webpackFinal: async (config) => {
+    const imageRule = config.module?.rules?.find((rule) => {
+      const test = (rule as { test: RegExp }).test;
+      if (!test) return false;
+      return test.test(".svg");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    }) as { [key: string]: any };
+
+    imageRule.exclude = /\.svg?$/;
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 export default config;
