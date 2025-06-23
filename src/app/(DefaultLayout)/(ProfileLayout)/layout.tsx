d763afa21ba39297652a58da1f185f7037ea8ee1
callu9 <callu_9ine@naver.com>
2025-06-23 12:23:26 +0900

    [GN-177] chore: metadata 설정 (제목 및 설명 추가)

--- src/app/(DefaultLayout)/(ProfileLayout)/layout.tsx
+++ src/app/(DefaultLayout)/(ProfileLayout)/layout.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
 import { ReactNode } from "react";
 
 import DynamicSideMenu from "@/components/sidemenu/DynamicSideMenu";
 import { cn } from "@/utils/classNames";
+
+export const metadata: Metadata = {
+  title: "GlobalNomad | 마이 페이지",
+  description: "나의 GlobalNomad 등록/신청 정보를 확인해 보세요",
+};
 
 function Layout({ children }: { children: ReactNode }) {
   return (
